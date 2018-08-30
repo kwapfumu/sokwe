8630b567ea8826befa8474e5754dce24d98936d6
kwapfumu <rigumyel@gmail.com>
2018-08-30 10:24:41 +0200

    updating testing files

--- cypress/integration/LoginForm/LoginForm.spec.js
+++ cypress/integration/LoginForm/LoginForm.spec.js
@@ -6,17 +6,29 @@ describe('testing loginForm', () => {
 
   it('displays a very long welcoming msg', () => {
     // eslint-disable-next-line no-undef
-    cy.contains('Alert > strong', 'Injiza email kugira tukurungikire muri email uruhusha rwo gutangura!');
+    cy.get('.loginFormRow').contains('strong', 'Injiza email kugira tukurungikire muri email uruhusha rwo gutangura!');
   });
 
-  it('requires an email', () => {
+  it('enters an email and clicks to submit it', () => {
+    // eslint-disable-next-line no-undef
+    // cy.get('.loginFormRow').contains('injiza email');
     // eslint-disable-next-line no-undef
     // cy.get('[data-test=email]').type('grr@gmail.com');
-    // cy.get('[data-test=email]').type('grr@gmail.com');
     // eslint-disable-next-line no-undef
+    cy.get('.loginFormRow').contains('Button', 'Ndungikira').click();
   });
 
   it('displays a login code msg', () => {
+    // eslint-disable-next-line no-undef
+    cy.get('.loginCodeFormRow').contains('strong', 'Injiza ibiharuro waronse muri email!');
+  });
 
+  it('enters a login code and redirects to isokoranye page', () => {
+    // eslint-disable-next-line no-undef
+    // cy.get('[data-test=email]').type('grr@gmail.com');
+    // eslint-disable-next-line no-undef
+    cy.get('.loginCodeFormRow').contains('Button', 'Twagiye').click();
+    // eslint-disable-next-line no-undef
+    cy.url().should('include', '/');
   });
 });
---
+++ sum.js
@@ -0,0 +1,5 @@
+const sum = (value1, value2) => (
+  value1 + value2
+);
+
+export default sum;
