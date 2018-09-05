702a983bf71537d04031ce7eaf7421704a1c7ce2
kwapfumu <rigumyel@gmail.com>
2018-09-05 16:41:15 +0200

    updating testing files

---
+++ cypress/integration/UsersList/UsersList.spec.js
@@ -0,0 +1,9 @@
+describe('Testing NavbarSokwe', () => {
+  beforeEach(() => {
+    // eslint-disable-next-line no-undef
+    cy.visit('/admin/users');
+  });
+
+  // rpt admin tests
+  it('loads a list of users', () => {});
+});
